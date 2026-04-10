1ca0eb9dfdc2014503f1ee05c962a7cc3f7b7e01
Tengda Fan <1012512411@qq.com>
2026-04-10 20:50:11 +0800

    test: 测试 多模态方法 (qwen max 不支持)

---
+++ src/main/java/com/threetwoa/aicodehelper/ai/AiCodeHelper.java
@@ -0,0 +1,43 @@
+package com.threetwoa.aicodehelper.ai;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AiCodeHelper {
+
+    @Resource
+    private ChatModel qwenChatModel;
+
+    /**
+     * 文本 消息测试
+     * @param message
+     * @return
+     */
+    public String chat(String message) {
+        UserMessage userMessage = UserMessage.from(message);
+        ChatResponse chatResponse = qwenChatModel.chat(userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("AI 输出：" + aiMessage.toString());
+        return aiMessage.text();
+    }
+
+    /**
+     * 多模态消息测试 (需要使用支持多模态的大模型)
+     * @param userMessage
+     * @return
+     */
+    public String chatWithMessage(UserMessage userMessage) {
+        ChatResponse chatResponse = qwenChatModel.chat(userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("AI 输出：" + aiMessage.toString());
+        return aiMessage.text();
+    }
+
+}
--- src/test/java/com/threetwoa/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/threetwoa/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,13 +1,38 @@
 package com.threetwoa.aicodehelper;
 
+import com.threetwoa.aicodehelper.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiCodeHelperApplicationTests {
 
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
+    /**
+     * 文本输出交互测试
+     */
     @Test
-    void contextLoads() {
+    void chat() {
+        aiCodeHelper.chat("hi, 我是threetwoa, 告诉我一个通俗的趣味的 spring 的知识");
+    }
+
+    /**
+     * 多模态 图片测试
+     */
+    @Test
+    void chatWithMessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                // 输出 编程导航的 logo 进行测试其多模态能力
+                ImageContent.from("https://www.codefather.cn/logo.png")
+        );
+        aiCodeHelper.chatWithMessage(userMessage);
     }
 
 }
